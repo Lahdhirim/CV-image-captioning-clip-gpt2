d7513254ded345ae8f878fe855a6d4f161294f68
Lahdhiri Moetez <moetez.lahdhiri@student-cs.fr>
2025-07-27 10:30:44 +0200

    Remove projector enhancement (does not improve results)

--- src/modeling/model.py
+++ src/modeling/model.py
@@ -28,11 +28,7 @@ class ClipCaptionModel(nn.Module):
         # Projection from CLIP embedding to GPT-2 input space
         # [LOW] add ProjectorConfig (hidden size, dropout rate) in config.json
         self.projector = nn.Sequential(
-            nn.Linear(clip_emb_dim, clip_emb_dim * 2),
-            nn.ReLU(),
-            nn.LayerNorm(clip_emb_dim * 2),
-            nn.Dropout(0.2),
-            nn.Linear(clip_emb_dim * 2, self.gpt_embedding_size * visual_tokens_length),
+            nn.Linear(clip_emb_dim, self.gpt_embedding_size * visual_tokens_length),
             nn.Tanh(),
         )
 
